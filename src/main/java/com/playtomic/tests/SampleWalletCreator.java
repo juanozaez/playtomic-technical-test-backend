269f2ef83e57b687583e3ef8b99a8969e481dd5d
Juanlu <juan.ozaez@code.seat>
2025-04-07 21:29:04 +0200

    Loader to init application with a sample Wallet

---
+++ src/main/java/com/playtomic/tests/SampleWalletCreator.java
@@ -0,0 +1,21 @@
+package com.playtomic.tests;
+
+import com.playtomic.tests.wallet.domain.Wallet;
+import com.playtomic.tests.wallet.domain.WalletId;
+import com.playtomic.tests.wallet.domain.WalletRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SampleWalletCreator {
+
+    @Autowired
+    private WalletRepository repository;
+
+    @EventListener(ApplicationReadyEvent.class)
+    void init(ApplicationReadyEvent event) {
+        repository.save(Wallet.emptyWallet(WalletId.fromString("2a727ad9-3243-483a-bf39-9840bbc0e459")));
+    }
+}
